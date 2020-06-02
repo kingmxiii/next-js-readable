43a1117b5f717373d5f24b1acfa42103607f8965
Rey Polanco <reypolanco@gmail.com>
2020-06-02 19:10:55 -0400

    initial commit

---
+++ components/App.js
@@ -0,0 +1,4 @@
+
+export default () => (
+    <div data-test="app-component"><h1>Welcome</h1></div>
+) 
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import App from '../components/App'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <App />
+    </div>
+  )
+}
