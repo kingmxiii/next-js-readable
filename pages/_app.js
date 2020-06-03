15d701b335211f8f4a427e7614e7b5ff5c3a63dd
Rey Polanco <reypolanco@gmail.com>
2020-06-03 11:34:03 -0400

    add bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
 import Layout from "../components/layout/layout";
 
 export default ({ Component, pageProps }) => (
   <Layout>
+    <Head>
+      {/* Add Bootstrap. Just for Simplicity */}
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossOrigin="anonymous"
+      />
+    </Head>
     <Component {...pageProps} />
   </Layout>
 );
